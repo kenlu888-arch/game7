d696b61ef2482862a3e8851fead9e88ac896138d
kenlu888-arch <kenlu888@gmail.com>
2025-12-18 21:05:01 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'number-treasure-vFINAL';
+const ASSETS = [
+    './',
+    './index.html',
+    './icon.jpg' 
+];
+
+self.addEventListener('install', (e) => {
+    self.skipWaiting(); 
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) return caches.delete(key);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => res || fetch(e.request))
+    );
+});
